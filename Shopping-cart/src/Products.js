caf885a819e2001ff16a8def0319bbd5caeb7bb1
iseacbrow <okonkwoisaacchuka@gmail.com>
2024-06-15 15:43:25 +0100

    Add files via upload

---
+++ Shopping-cart/src/Products.js
@@ -0,0 +1,110 @@
+/*============ Build our products data ==============*/
+
+export const productsData = [
+  {
+    id: 1,
+    title: "moses",
+    price: 2000,
+    imageUrl:
+      "/images/c1.jpg",
+  },
+
+  {
+    id: 2,
+    title: "Carton Astronaut T-Shirt",
+    price: 300,
+    imageUrl: "/images/c2.jpg",
+  },
+
+  {
+    id: 3,
+    title: "Carton Astronaut T-Shirt",
+    price: 300,
+    imageUrl: "/images/c3.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Carton Astronaut T-Shirt",
+    price: 95,
+    imageUrl: "/images/c4.jpg",
+  },
+
+  {
+    id: 5,
+    title: "Carton Astronaut T-Shirt",
+    price: 3500,
+    imageUrl:
+    "/images/c5.jpg",
+  },
+
+  {
+    id: 6,
+    title: "Carton Astronaut T-Shirt",
+    price: 12,
+    imageUrl: "/images/c6.jpg",
+  },
+
+  {
+    id: 7,
+    title: "Carton Astronaut T-Shirt",
+    price: 500,
+    imageUrl: "/images/c7.jpg",
+  },
+
+  {
+    id: 8,
+    title: "Carton Astronaut T-Shirt",
+    price: 500,
+    imageUrl: "/images/c8.jpg",
+  },
+
+  {
+    id: 9,
+    title: "Carton Astronaut T-Shirt",
+    price: 129,
+    imageUrl: "/images/c9.jpg",
+  },
+
+  {
+    id: 10,
+    title: "Carton Astronaut T-Shirt",
+    price: 650,
+    imageUrl: "/images/c10.jpg",
+  },
+
+  {
+    id: 11,
+    title: "Carton Astronaut T-Shirt",
+    price: 700,
+    imageUrl: "/images/ch11.jpg",
+  },
+
+  {
+    id: 12,
+    title: "Carton Astronaut T-Shirt",
+    price: 119,
+    imageUrl: "/images/ch12.jpg",
+  },
+
+  {
+    id: 13,
+    title: "Carton Astronaut T-Shirt",
+    price: 400,
+    imageUrl: "/images/ch13.jpg",
+  },
+
+  {
+    id: 14,
+    title: "Carton Astronaut T-Shirt",
+    price: 500,
+    imageUrl: "/images/ch14.jpg",
+  },
+
+  {
+    id: 15,
+    title: "Carton Astronaut T-Shirt",
+    price: 350,
+    imageUrl: "/images/ch15.jpg",
+  },
+];
